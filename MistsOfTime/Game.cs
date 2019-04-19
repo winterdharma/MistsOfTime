11f663ac0698e711f58af502477da52fe37e4ae2
Charlie Patton <cdpatton2009@gmail.com>
2019-04-19 00:13:20 -0700

    Refactored movement input and added support for both direction letters and arrow keys.

--- MistsOfTime/Game.cs
+++ MistsOfTime/Game.cs
@@ -10,10 +10,16 @@ namespace MistsOfTime
 {
     public class Game
     {
+        private ConsoleKey[] _moveKeys = new ConsoleKey[] 
+            { ConsoleKey.N, ConsoleKey.S, ConsoleKey.E, ConsoleKey.W,
+              ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.RightArrow,
+              ConsoleKey.LeftArrow };
+
         public Game()
         {
             World = new World(20, 20);
             Here = World.GetStartingLocation();
+            ActionResult = string.Empty;
         }
 
         public World World { get; set; }
@@ -29,45 +35,50 @@ namespace MistsOfTime
             {
                 Console.Clear();
 
-                Console.WriteLine(ActionResult);
-                ActionResult = string.Empty;
+                if (ActionResult.Length > 0)
+                {
+                    Console.WriteLine(ActionResult);
+                    ActionResult = string.Empty;
+                }
 
                 Console.WriteLine(Here.Coords + " : " + Here.Description);
 
                 var input = Console.ReadKey();
 
-                Location newPlace = null;
-                if (input.Key == ConsoleKey.N)
-                {
-                    if (World.MoveNorth(Here, out newPlace))
-                        Here = newPlace;
-                    else
-                        ActionResult = LocationStrings.MovedOutOfBounds;
-                }
-                else if(input.Key == ConsoleKey.E)
-                {
-                    if (World.MoveEast(Here, out newPlace))
-                        Here = newPlace;
-                    else
-                        ActionResult = LocationStrings.MovedOutOfBounds;
-                }
-                else if(input.Key == ConsoleKey.S)
-                {
-                    if (World.MoveSouth(Here, out newPlace))
-                        Here = newPlace;
-                    else
-                        ActionResult = LocationStrings.MovedOutOfBounds;
-                }
-                else if(input.Key == ConsoleKey.W)
-                {
-                    if (World.MoveWest(Here, out newPlace))
-                        Here = newPlace;
-                    else
-                        ActionResult = LocationStrings.MovedOutOfBounds;
-                }
+                if (_moveKeys.Contains(input.Key))
+                    Move(input.Key);
+                
                 else if (input.Key == ConsoleKey.Escape)
                     IsGameOn = false;
             }
+        }
+
+        private void Move(ConsoleKey dir)
+        {
+            Location newPlace = Here;
+            bool isValidMove = false;
+            switch(dir)
+            {
+                case ConsoleKey.N:
+                case ConsoleKey.UpArrow:
+                    isValidMove = World.MoveNorth(Here, out newPlace);
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    isValidMove = World.MoveSouth(Here, out newPlace);
+                    break;
+                case ConsoleKey.E:
+                case ConsoleKey.RightArrow:
+                    isValidMove = World.MoveEast(Here, out newPlace);
+                    break;
+                case ConsoleKey.W:
+                case ConsoleKey.LeftArrow:
+                    isValidMove = World.MoveWest(Here, out newPlace);
+                    break;
+            }
+            Here = newPlace;
+            if(!isValidMove)
+                ActionResult = LocationStrings.MovedOutOfBounds;
         }
     }
 }
